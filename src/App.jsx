e813cf63f66621741d6a0beb3f8fd2dd4ee2ab5f
Akash2709 <56739695+Akash2709@users.noreply.github.com>
2023-03-23 16:18:38 +0530

    display X when clicked on a square

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,9 @@ import "./styles.scss";
 import Board from "./components/Board";
 
 function App() {
-  const returnValue = useState(1);
-  console.log(returnValue);
-
-  let counter = 1;
-  const onBtnClick = () => {
-    console.log("hello");
-    counter = counter + 1;
-  };
   return (
     <div className="app">
-      <div>
-        <button onClick={onBtnClick}>Click me please</button>
-        <div>{counter}</div>
-      </div>
+      <Board />
     </div>
   );
 }
