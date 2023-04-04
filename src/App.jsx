958eb4b93d1cfa0ee4521efbcdb0a53afbd51ee9
Akash2709 <56739695+Akash2709@users.noreply.github.com>
2023-04-04 11:36:15 +0530

    display draw message . apply styles for StatusMessage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import "./styles.scss";
+import { useState } from "react";
 import Board from "./components/Board";
+import StatusMessage from "./components/StatusMessage";
+import { calculateWinner } from "./winner";
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(squares);
+  // eslint-disable-next-line no-unused-vars
+
+  const handleSquareClick = (clickedPosition) => {
+    if (squares[clickedPosition]) {
+      return;
+    }
+
+    setSquares((currentSquares) => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? `X` : `O`;
+        }
+
+        return squareValue;
+      });
+    });
+
+    setIsXNext((currentIsNext) => !currentIsNext);
+  };
+
   return (
     <div className="app">
-      <Board />
+      <StatusMessage winner={winner} isXNext={isXNext} squares={squares} />
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
